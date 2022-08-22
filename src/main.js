44c3d8b6621f74a915aa07fe5fb75cf8be963758
CodingRafi <codingmuhamadrafiprasetya@gmail.com>
2022-08-22 22:12:03 +0700

    gagal

--- src/main.js
+++ src/main.js
@@ -8,12 +8,19 @@ import 'primevue/resources/themes/saga-blue/theme.css';     //theme
 import 'primevue/resources/primevue.min.css';               //core css
 import 'primeicons/primeicons.css';                           //icons
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(PrimeVue);
+app.use(BootstrapVue)
+app.use(IconsPlugin)
+app.use(PrimeVue)
 app.use(router)
 
 app.mount('#app')
